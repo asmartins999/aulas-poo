f45ebbc75b25ceeb0b41f1c94c73df477e0c08e7
Anderson Soares <asmartins999@gmail.com>
2020-04-07 07:42:54 -0300

    Adicionando aula 07, index, class e notas no arquivo readme.md

---
+++ aula_07/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'class.lutador.php';
+
+$l = array();
+$l[0] = new Lutador("Pretty Boy", "Francês", 31, 1.75, 68.9, 11, 2, 1);
+$l[1] = new Lutador("PutScript","Brasileiro",29,1.68,57.8,14,2,3);
+
+$l[0]->apresentar();
+$l[1]->apresentar();
+
+echo "<pre>";
+print_r($l);
+
+
+
+
+
+
+?>
